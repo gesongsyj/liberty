943f4670a4a34eaae2cc282f1eabf67939dff446
宋焰君 <1971119509@qq.com>
2018-07-13 17:46:42 +0800

    111

--- src/main/java/com/liberty/common/utils/HexunUtil.java
+++
@@ -1,5 +0,0 @@
-package com.liberty.common.utils;
-
-public class HexunUtil {
-
-}
--- src/main/java/com/liberty/system/web/CurrencyController.java
+++ src/main/java/com/liberty/system/web/CurrencyController.java
@@ -56,7 +56,7 @@ public class CurrencyController extends BaseController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		renderText(response);
+		renderText(response);
 	}
 
 }
